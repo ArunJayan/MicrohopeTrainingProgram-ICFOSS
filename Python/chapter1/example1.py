0e73a782a269ace0581de3f3b0102a8baacd1348
ArunJayan <arunjayan32@gmail.com>
2015-03-13 00:50:01 +0530

    modified example1.py --- some examples for print

---
+++ Python/chapter1/example1.py
@@ -0,0 +1,5 @@
+# example1.property
+# This example will print "Hello World " message using  'print'
+print "Hello World"
+print "My Name is %s"%("Arun")	#this will show "My Name is Arun"
+print "%d+%d=%d"%(23,55,23+55) #this will show 23+55=78
