126cc1556b265e869ecae241cbd36e4046a1fbe2
ArunJayan <arunjayan32@gmail.com>
2015-03-13 01:01:14 +0530

    In this example we discuss about variables

---
+++ Python/chapter1/example2.py
@@ -0,0 +1,15 @@
+# in this example we are going to use variables 
+# first we can create two variables named num1 and num2 
+# and we will store two values on these two variable 
+# num1 = 300 
+# num2 = 230
+# sum = num1+num2
+#########
+# in C,C++ and Java before a variable is going to be used , 
+# declare that variable means we have to specify the type of variable.
+###But in Python , playing with variable is simple . Becoz we dont have to decalre it .
+###In python dynamic declaration is used .
+#########
+
+
+
