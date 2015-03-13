58903863138694c2d61a35cc32bb35b96d14227c
ArunJayan <arunjayan32@gmail.com>
2015-03-14 00:10:47 +0530

    modified little

--- Python/chapter1/example1.py
+++ Python/chapter1/example1.py
@@ -1,5 +1,7 @@
 # example1.property
 # This example will print "Hello World " message using  'print'
 print "Hello World"
+print "IEEE "
+print "ICFOSS"
 print "My Name is %s"%("Arun")	#this will show "My Name is Arun"
 print "%d+%d=%d"%(23,55,23+55) #this will show 23+55=78
