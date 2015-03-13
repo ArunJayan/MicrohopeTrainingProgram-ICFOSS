04a73fbc9f631be4d857317b896e09e846893723
ArunJayan <arunjayan32@gmail.com>
2015-03-14 00:24:34 +0530

    Read ata from keyboard

---
+++ Python/chapter1/example3.py
@@ -0,0 +1,8 @@
+#in this example we discuss hpow we read inputs from keyboard . 
+#first we see how we use input() then we will move onto raw_input()
+
+num1 = input("Enter num1 :: ")
+#in the above step we read a integer and store it into variable num1
+num2 = input("Enter num2 :: ")
+#in the above step we read another integer and store it into variable num2
+print "%d + %d = %d"%(num1,num2,num1+num2)
